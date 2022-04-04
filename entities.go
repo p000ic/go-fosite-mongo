19736a63fe7fda940bf8b483fa8d5c9b47dfbc2a
p000ic <pjanard@meghcomputing.com>
2022-04-04 00:15:50 -0400

    model update.

--- entities.go
+++ entities.go
@@ -1,35 +1,36 @@
 package storage
 
 const (
+	CollectionPrefix = "oauth2_"
 	// EntityOpenIDSessions provides the name of the entity to use in order to
 	// create, read, update and delete OpenID Sessions.
-	EntityOpenIDSessions = "openidconnectsession"
+	EntityOpenIDSessions = CollectionPrefix + "openid_connect_session"
 
 	// EntityAccessTokens provides the name of the entity to use in order to
 	// create, read, update and delete Access Token sessions.
-	EntityAccessTokens = "accesstoken"
+	EntityAccessTokens = CollectionPrefix + "access_token"
 
 	// EntityRefreshTokens provides the name of the entity to use in order to
 	// create, read, update and delete Refresh Token sessions.
-	EntityRefreshTokens = "refreshtoken"
+	EntityRefreshTokens = CollectionPrefix + "refresh_token"
 
 	// EntityAuthorizationCodes provides the name of the entity to use in order
 	// to create, read, update and delete Authorization Code sessions.
-	EntityAuthorizationCodes = "authorizationcode"
+	EntityAuthorizationCodes = CollectionPrefix + "authorization_code"
 
 	// EntityPKCESessions provides the name of the entity to use in order to
 	// create, read, update and delete Proof Key for Code Exchange sessions.
-	EntityPKCESessions = "pkcesession"
+	EntityPKCESessions = CollectionPrefix + "pkce_session"
 
 	// EntityJtiDenylist provides the name of the entity to use in order to
 	// track and deny.
-	EntityJtiDenylist = "jtidenylist"
+	EntityJtiDenylist = CollectionPrefix + "jti_deny_list"
 
 	// EntityClients provides the name of the entity to use in order to create,
 	// read, update and delete Clients.
-	EntityClients = "client"
+	EntityClients = CollectionPrefix + "client"
 
 	// EntityUsers provides the name of the entity to use in order to create,
 	// read, update and delete Users.
-	EntityUsers = "user"
+	EntityUsers = CollectionPrefix + "user"
 )
